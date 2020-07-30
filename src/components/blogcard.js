1376797c315153c6df5a5cda63b690f70712eb5d
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-07-30 13:06:14 +0530

    blog structure added

--- src/components/blogcard.js
+++ src/components/blogcard.js
@@ -3,10 +3,16 @@ import Blogimage from "../images/rove.png"
 export default function Blogcard() {
   return (
     <div className="blog-card">
-      <img src={Blogimage} className="blog-image" />
-      <h2 className="m-0">Blog name 1</h2>
-      <p className="m-0">Anurag Gharat</p>
-      <button>Read on Medium</button>
+      <a href="#">
+        <img src={Blogimage} className="blog-image" />
+      </a>
+      <h2 className="m-0">Blog Title 1</h2>
+      <p className="m-0">12 July 2020</p>
+      <ul className="text-wrap">
+        <li className="chip">node</li>
+        <li className="chip">node</li>
+        <li className="chip">node</li>
+      </ul>
     </div>
   )
 }
