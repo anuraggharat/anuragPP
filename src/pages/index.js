d44af03e8e00adc616d1055da79d55b4f920b041
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-08-05 15:31:29 +0530

    index page content added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,20 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa"
+// import Anurag from "../images/anurag.png"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Anurag Gharat" />
     <div className="container">
       <div className="home">
         <div className="col-lg-6">
           <h1 className="intro-line">
-            Hey There! I am <br /> <span className="name">Anurag!</span>
+            Hey There, I am <br /> <span className="name">Anurag!</span>
           </h1>
-          <p>A Front-End Designer and Developer based in Mumbai!</p>
-          <p>Currently Looking projects and Internships to work on!</p>
+          <p>A Front-End Developer based in Mumbai.</p>
+          <p>I create products for the Web with great User Experience.</p>
+          <p>Currently looking for Projects and Internships to work on!</p>
           <p>Want to get in touch?Contact me here!</p>
           <div className="social-media">
             <a href="anuraggharat.me" className="social-icons">
