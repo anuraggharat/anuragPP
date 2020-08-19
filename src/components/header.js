544fa168cb2dc47bf59c22f01d1977e71877c767
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-08-19 20:38:03 +0530

    navigation bar done

--- src/components/header.js
+++ src/components/header.js
@@ -12,33 +12,45 @@ const Header = () => (
         </h2>
       </div>
       <div className="navigation">
-        <Link className="nav-link" to="/about">
+        <Link className="nav-link" to="/about" activeClassName="nav-active">
           About
         </Link>
-        <Link className="nav-link" to="/blogs">
+        <Link className="nav-link" to="/blogs" activeClassName="nav-active">
           Blogs
         </Link>
-        <Link className="nav-link" to="/work">
+        <Link className="nav-link" to="/work" activeClassName="nav-active">
           Works
         </Link>
-        <Link className="nav-link" to="/contact">
+        <Link className="nav-link" to="/contact" activeClassName="nav-active">
           Contact
         </Link>
       </div>
       <div className="navigation-icons">
-        <Link className="nav-link" to="/">
+        <Link className="nav-link" to="/" activeClassName="nav-link-active">
           <FaHome className="navigation-icon" />
         </Link>
-        <Link className="nav-link" to="/about">
+        <Link
+          className="nav-link"
+          to="/about"
+          activeClassName="nav-link-active"
+        >
           <FaUserGraduate className="navigation-icon" />
         </Link>
-        <Link className="nav-link" to="/blogs">
+        <Link
+          className="nav-link"
+          to="/blogs"
+          activeClassName="nav-link-active"
+        >
           <FaPenFancy className="navigation-icon" />
         </Link>
-        <Link className="nav-link" to="/work">
+        <Link className="nav-link" to="/work" activeClassName="nav-link-active">
           <MdLaptopMac className="navigation-icon" />
         </Link>
-        <Link className="nav-link" to="/contact">
+        <Link
+          className="nav-link"
+          to="/contact"
+          activeClassName="nav-link-active"
+        >
           <FaPhoneAlt className="navigation-icon" />
         </Link>
       </div>
